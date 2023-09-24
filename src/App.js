f723c7600b6759bda7d9cb16cb73a50a2ed003e8
Aditya <aditya2000learning@gmail.com>
2023-09-24 08:04:43 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <h1>Sample React JS Application</h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/aditya2000learning/react-app-example1.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Link for the Project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
